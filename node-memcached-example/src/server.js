aa0fd6cb670a1f6b25e38272b67ce686b4e0eee0
Sanjalee Patni <156739014+Sanjalee-NewRelic@users.noreply.github.com>
2024-05-06 14:56:38 +0530

    Add files via upload

---
+++ node-memcached-example/src/server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const path = require("path");
+const routes = require("./api/routes");
+
+const Memcached = require('memcached');
+
+
+//Connect to memcache
+
+ //Memcache connection
+ const client= new Memcached('nodejs-memcached-test-cache.xutotu.cfg.eun1.cache.amazonaws.com:11211');
+ client.connect('nodejs-memcached-test-cache.xutotu.cfg.eun1.cache.amazonaws.com:11211', function( err, conn ){
+  if( err ) 
+  {
+    console.log('error connecting to memcached', err);
+  }
+  else{
+    
+    console.log( 'conn server',conn.server );
+  }
+});
+
+client.on('issue', function(details) {
+     console.log('issue details',details)
+ });
+
+client.set('key', 'value', 10000, function(err) {
+  console.log('insoide the cb')
+   if (err) {
+       console.error('Error setting value:', err);
+       return;
+   }
+   console.log('Value set successfully');
+});
+
+client.get('key', function(err, data) {
+ if (err) {
+     console.error('Error getting value:', err);
+     return;
+ }
+ console.log('Value retrieved:', data);
+});
+
+const app = express();
+
+// Use the API routes before serving the static files
+app.use("/api", routes);
+
+// Serve static files from the public folder
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
