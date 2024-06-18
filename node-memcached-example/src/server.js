364d554b6f7f45f9b103432468c94b3923955dd0
Sanjalee Patni <156739014+Sanjalee-NewRelic@users.noreply.github.com>
2024-06-18 16:27:15 +0530

    Connect to memcached on get request

--- node-memcached-example/src/server.js
+++ node-memcached-example/src/server.js
@@ -50,6 +50,42 @@ app.use("/api", routes);
 app.use(express.static(path.join(__dirname, "../dist")));
 
 app.get("*", (_, res) => {
+
+ //Connect to memcache
+
+ //Memcache connection
+ const client= new Memcached('memcached-cluster-for-node-app.kj3yin.cfg.use1.cache.amazonaws.com:11211');
+ client.connect('memcached-cluster-for-node-app.kj3yin.cfg.use1.cache.amazonaws.com:11211', function( err, conn ){
+  if( err ) 
+  {
+    console.log('error connecting to memcached', err);
+  }
+  else{
+    
+    console.log( 'conn server',conn.server );
+  }
+});
+
+client.on('issue', function(details) {
+     console.log('issue details',details)
+ });
+
+client.set('key', 'value', 10000, function(err) {
+  console.log('insoide the cb')
+   if (err) {
+       console.error('Error setting value:', err);
+       return;
+   }
+   console.log('Value set successfully');
+});
+
+client.get('key', function(err, data) {
+ if (err) {
+     console.error('Error getting value:', err);
+     return;
+ }
+ console.log('Value retrieved:', data);
+});
   res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
 
